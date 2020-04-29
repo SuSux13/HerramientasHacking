0d9f0263bab9740b2d97d82e55b3e17f15179696
SuSux13 <55140831+SuSux13@users.noreply.github.com>
2020-04-29 17:22:24 +0200

    Create Nªmail.kt

---
+++ Nªmail.kt
@@ -0,0 +1,14 @@
+/*
+The number you add will be the number of times the email will reach your contact.
+*/
+
+fun main(args: Array<String>) {
+    var mail = 0;
+
+    while(mail <= 5) {
+        println(mail);
+        mail++;
+    }  
+}
+
+
